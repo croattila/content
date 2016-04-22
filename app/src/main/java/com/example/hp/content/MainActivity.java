c056d312c824fc93a45a5968038b44688df78026
Attila Horvath <croattila@yahoo.com>
2016-04-22 20:13:37 +0100

    Masodik, vegso verzio

--- app/src/main/java/com/example/hp/content/MainActivity.java
+++ app/src/main/java/com/example/hp/content/MainActivity.java
@@ -6,9 +6,19 @@ import android.os.Bundle;
 import android.provider.UserDictionary;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.support.v4.widget.SimpleCursorAdapter;
 
 public class MainActivity extends ActionBarActivity {
+    private static final String[] COLUMNS_TO_BE_BOUND = new String[]{
+            UserDictionary.Words.WORD,
+            UserDictionary.Words.FREQUENCY,
+
+    };
+    private static final int[] LAYOUT_ITEMS_TO_FILL = new int[]{
+            android.R.id.text1,
+            android.R.id.text2
+    };
 
 
     @Override
@@ -17,34 +27,22 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
 
-        TextView dictTextView = (TextView) findViewById(R.id.dictionary_text_view);
+        ListView dictListView = (ListView) findViewById(R.id.dictionary_list_view);
 
         ContentResolver resolver = getContentResolver();
 
         Cursor cursor = resolver.query(UserDictionary.Words.CONTENT_URI, null, null, null, null);
         Log.e("TEST", UserDictionary.Words.CONTENT_URI.toString());
 
+        SimpleCursorAdapter adapter= new SimpleCursorAdapter(this,
+                android.R.layout.two_line_list_item,
+                cursor,
+                COLUMNS_TO_BE_BOUND,
+                LAYOUT_ITEMS_TO_FILL,
+                0);
 
-        try {
-            dictTextView.setText(" A Szotarban van " + cursor.getCount() + " szo\n");
-            dictTextView.append("Sorok: " + UserDictionary.Words._ID + " - " + UserDictionary.Words.FREQUENCY
-                    + " - " + UserDictionary.Words.WORD);
-
-            int idColumn = cursor.getColumnIndex(UserDictionary.Words._ID);
-            int frequencyColumn = cursor.getColumnIndex(UserDictionary.Words.FREQUENCY);
-            int wordColumn = cursor.getColumnIndex(UserDictionary.Words.WORD);
-
-            while (cursor.moveToNext()) {
-
-                int id = cursor.getInt(idColumn);
-                int frequency = cursor.getInt(frequencyColumn);
-                String word = cursor.getString(wordColumn);
-
-                dictTextView.append(("\n" + id + " - " + frequency + " - " + word));
-            }
-        } finally {
-            //cursor.close();
-        }
+    dictListView.setAdapter(adapter);
+//        cursor.close();
 
 
     }
